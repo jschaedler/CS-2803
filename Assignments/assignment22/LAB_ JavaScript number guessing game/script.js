14bbf37aefa2867d499c7d1710768859dba97c5f
jschaedler <jschaedler7@gmail.com>
2022-02-25 15:25:51 -0500

    22 done

--- Assignments/assignment22/LAB_ JavaScript number guessing game/script.js
+++ Assignments/assignment22/LAB_ JavaScript number guessing game/script.js
@@ -4,6 +4,7 @@ console.log("Answer:", window.numToGuess); //For you to know what the number is.
 function playGuessingGame(totalGuesses = 10) {
    let promptText = "Enter a number between 1 and 100.";
    //Your answer goes here.
+   
 
    /*
       Do not use alert() to let the user know of new messages
@@ -14,11 +15,30 @@ function playGuessingGame(totalGuesses = 10) {
    */
    
    /*implement loop*/
+   let tries = 0
+   while (totalGuesses > 0) {
+      tries++
       let num = prompt(promptText)
+      if (num === null) {
+         totalGuesses = 0
+         return 0
+      }
       
       if (num === "" || isNaN(num)) {
-            promptText = "Please enter a number.";
-      }
+         promptText = "Please enter a number.";
+         totalGuesses++
+         tries--
+      } else if (num < window.numToGuess) {
+         promptText = num + " is too small. Guess a larger number."
+      } else if (num > window.numToGuess) {
+         promptText = num + " is too large. Guess a smaller number."
+      } else {
+         totalGuesses = 0
 
-   
+         return tries
+      }
+      totalGuesses--
+
+   }
+   return 0
 }
